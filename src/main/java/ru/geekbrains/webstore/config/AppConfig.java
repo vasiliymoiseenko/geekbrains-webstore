32874170af9b80dc0ccb9ed40187464e40e036bc
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-04-22 14:31:09 +0500

    Добавление AppConfig

---
+++ src/main/java/ru/geekbrains/webstore/config/AppConfig.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.webstore.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("ru.geekbrains.webstore")
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources");
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+}
---
+++ src/main/webapp/header.html
@@ -0,0 +1,1 @@
+<h1>Header file</h1>
