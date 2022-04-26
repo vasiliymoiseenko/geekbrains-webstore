f6edd150cf30af92d7a42f79472de96ec2ffd34b
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-04-26 11:47:28 +0500

    Доработан ProductRepository
    - оптимизирован метод deleteById(Long id)
    - оптимизирован метод update(Product product)

--- src/main/java/ru/geekbrains/webstore/repositories/ProductRepository.java
+++ src/main/java/ru/geekbrains/webstore/repositories/ProductRepository.java
@@ -4,23 +4,24 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import ru.geekbrains.webstore.entities.Product;
 
 @Component
-@Primary
+//@Primary
 @AllArgsConstructor
 public class ProductRepository implements Repository<Product>{
 
   private final EntityManager entityManager;
 
   @Override
+  @Transactional
   public Product findById(Long id) {
     return entityManager.find(Product.class, id);
   }
 
   @Override
+  @Transactional
   public List<Product> findAll() {
     return entityManager
             .createQuery("Select a from Product a", Product.class)
@@ -30,8 +31,10 @@ public class ProductRepository implements Repository<Product>{
   @Override
   @Transactional
   public void deleteById(Long id) {
-    Product product = findById(id);
-    entityManager.remove(product);
+    entityManager
+            .createQuery("delete from Product a where a.id = :id")
+            .setParameter("id", id)
+            .executeUpdate();
   }
 
   @Override
@@ -43,9 +46,6 @@ public class ProductRepository implements Repository<Product>{
   @Override
   @Transactional
   public void update(Product product) {
-    Product productUpdate = findById(product.getId());
-    productUpdate.setPrice(product.getPrice());
-    productUpdate.setTitle(product.getTitle());
-    entityManager.merge(productUpdate);
+    entityManager.merge(product);
   }
 }
