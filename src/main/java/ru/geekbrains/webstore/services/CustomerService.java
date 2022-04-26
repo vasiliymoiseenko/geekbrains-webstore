07fa6449ccc7db5c1b0991217c6076ee641d2b63
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-04-26 12:10:40 +0500

    Доработан CustomerService

---
+++ src/main/java/ru/geekbrains/webstore/services/CustomerService.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.webstore.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.webstore.entities.Customer;
+import ru.geekbrains.webstore.repositories.CustomerRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CustomerService implements ru.geekbrains.webstore.services.Service<Customer> {
+
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void update(Customer customer) {
+        customerRepository.update(customer);
+    }
+}
