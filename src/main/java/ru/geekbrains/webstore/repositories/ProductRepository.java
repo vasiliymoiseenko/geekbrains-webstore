307624b302a4a66f7cf5fa2c42620ca03a48ff73
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-04-29 09:16:31 +0500

    Рефактор

--- src/main/java/ru/geekbrains/webstore/repositories/ProductRepository.java
+++ src/main/java/ru/geekbrains/webstore/repositories/ProductRepository.java
@@ -1,17 +1,16 @@
 package ru.geekbrains.webstore.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.webstore.entities.Product;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-  List<Product> findAllByPriceBefore(Double maxPrice);
+  List<Product> findAllByPriceIsLessThanEqual(Double maxPrice);
 
-  List<Product> findAllByPriceAfter(Double minPrice);
+  List<Product> findAllByPriceGreaterThanEqual(Double minPrice);
 
   List<Product> findAllByPriceBetween(Double minPrice, Double maxPrice);
 }
--- src/main/java/ru/geekbrains/webstore/services/ProductService.java
+++ src/main/java/ru/geekbrains/webstore/services/ProductService.java
@@ -1,10 +1,8 @@
 package ru.geekbrains.webstore.services;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.webstore.entities.Product;
@@ -38,13 +36,14 @@ public class ProductService implements ru.geekbrains.webstore.services.Service<P
 
   public List<Product> findAllByPrice(Double minPrice, Double maxPrice) {
     if (minPrice == null && maxPrice == null) {
-      return productRepository.findAll();
-    } else if (minPrice != null && maxPrice != null) {
-      return productRepository.findAllByPriceBetween(minPrice, maxPrice);
-    } else if (minPrice == null) {
-      return productRepository.findAllByPriceBefore(maxPrice);
-    } else {
-      return productRepository.findAllByPriceAfter(minPrice);
+      return Collections.emptyList();
     }
+    if (minPrice != null && maxPrice != null) {
+      return productRepository.findAllByPriceBetween(minPrice, maxPrice);
+    }
+    if (minPrice == null) {
+      return productRepository.findAllByPriceIsLessThanEqual(maxPrice);
+    }
+    return productRepository.findAllByPriceGreaterThanEqual(minPrice);
   }
 }
