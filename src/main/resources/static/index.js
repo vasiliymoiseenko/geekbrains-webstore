6ef79db4d90dec479bfd81c7b985d47fe8234a34
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-05-27 17:06:14 +0500

    Добавлены токены и форма логина

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,6 +1,6 @@
 (function () {
   angular
-  .module('webstore-front', ['ngRoute'])
+  .module('webstore-front', ['ngRoute', 'ngStorage'])
   .config(config)
   .run(run);
 
@@ -31,12 +31,58 @@
     });
   }
 
-  function run($rootScope, $http) {
+  function run($rootScope, $http, $localStorage) {
+    if ($localStorage.webMarketUser) {
+      $http.defaults.headers.common.Authorization = 'Bearer '
+          + $localStorage.webstoreUser.token;
+    }
   }
 })();
 
 angular.module('webstore-front').controller('indexController',
-    function ($rootScope, $scope, $http) {
+    function ($rootScope, $scope, $http, $localStorage) {
       const contextPath = 'http://localhost:8189/webstore';
+
+      $scope.tryToAuth = function () {
+        $http.post(contextPath + '/auth', $scope.user)
+        .then(function successCallback(response) {
+          if (response.data.token) {
+            $http.defaults.headers.common.Authorization = 'Bearer '
+                + response.data.token;
+            $localStorage.webstoreUser = {
+              username: $scope.user.username,
+              token: response.data.token
+            };
+
+            $scope.user.username = null;
+            $scope.user.password = null;
+          }
+        }, function errorCallback(response) {
+          alert(response.data.messages)
+        });
+      };
+
+      $scope.tryToLogout = function () {
+        $scope.clearUser();
+        if ($scope.user.username) {
+          $scope.user.username = null;
+        }
+        if ($scope.user.password) {
+          $scope.user.password = null;
+        }
+      };
+
+      $scope.clearUser = function () {
+        delete $localStorage.webstoreUser;
+        $http.defaults.headers.common.Authorization = '';
+      };
+
+      $rootScope.isUserLoggedIn = function () {
+        if ($localStorage.webstoreUser) {
+          return true;
+        } else {
+          return false;
+        }
+      };
 
     });
