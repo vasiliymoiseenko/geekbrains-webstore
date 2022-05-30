e535987e009348bb4cf00287ab14aa294c97faff
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-05-30 16:00:18 +0500

    Доработка OrderMapper

--- src/main/java/ru/geekbrains/webstore/mapper/OrderMapper.java
+++ src/main/java/ru/geekbrains/webstore/mapper/OrderMapper.java
@@ -5,12 +5,9 @@ import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import ru.geekbrains.webstore.dto.OrderDto;
 import ru.geekbrains.webstore.entity.Order;
-import ru.geekbrains.webstore.entity.Product;
-import ru.geekbrains.webstore.entity.User;
 import ru.geekbrains.webstore.exception.ResourceNotFoundException;
 import ru.geekbrains.webstore.service.ProductService;
 import ru.geekbrains.webstore.service.UserService;
@@ -20,19 +17,9 @@ public interface OrderMapper {
 
   OrderMapper ORDER_MAPPER = Mappers.getMapper(OrderMapper.class);
 
-  @Mapping(target = "username", source = "user", qualifiedByName = "userToUsername")
-  @Mapping(target = "productTitle", source = "product", qualifiedByName = "productToTitle")
+  @Mapping(target = "username", expression = "java(order.getUser().getUsername())")
+  @Mapping(target = "productTitle", expression = "java(order.getProduct().getTitle())")
   OrderDto fromOrder(Order order);
-
-  @Named("userToUsername")
-  default String userToUsername(User user) {
-    return user.getUsername();
-  }
-
-  @Named("productToTitle")
-  default String productToTitle(Product product) {
-    return product.getTitle();
-  }
 
   @Mapping(target = "user", ignore = true)
   @Mapping(target = "product", ignore = true)
