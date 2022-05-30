2c959556e45022500d4b7ea36f72c8bf4784e7b0
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-05-30 14:09:35 +0500

    Доработка OrderMapper

--- src/main/java/ru/geekbrains/webstore/mapper/OrderMapper.java
+++ src/main/java/ru/geekbrains/webstore/mapper/OrderMapper.java
@@ -1,14 +1,12 @@
 package ru.geekbrains.webstore.mapper;
 
-import java.util.List;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import ru.geekbrains.webstore.dto.OrderDto;
-import ru.geekbrains.webstore.dto.UserDto;
 import ru.geekbrains.webstore.entity.Order;
+import ru.geekbrains.webstore.entity.Product;
 import ru.geekbrains.webstore.entity.User;
 
 @Mapper
@@ -17,10 +15,16 @@ public interface OrderMapper {
   OrderMapper ORDER_MAPPER = Mappers.getMapper(OrderMapper.class);
 
   @Mapping(target = "username", source = "user", qualifiedByName = "userToUsername")
+  @Mapping(target = "productTitle", source = "product", qualifiedByName = "productToTitle")
   OrderDto fromOrder(Order order);
 
   @Named("userToUsername")
   default String userToUsername(User user) {
     return user.getUsername();
+  }
+
+  @Named("productToTitle")
+  default String productToTitle(Product product) {
+    return product.getTitle();
   }
 }
