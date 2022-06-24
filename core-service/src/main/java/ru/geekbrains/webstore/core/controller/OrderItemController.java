c3900d1d5eb03a55667f7e1155d6ac519112ffa0
vasiliymoiseenko <vasiliymoiseenko@yandex.ru>
2022-06-24 17:26:46 +0500

    Исправление багов

--- core-service/src/main/java/ru/geekbrains/webstore/core/controller/OrderItemController.java
+++ core-service/src/main/java/ru/geekbrains/webstore/core/controller/OrderItemController.java
@@ -1,18 +1,17 @@
 package ru.geekbrains.webstore.core.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import ru.geekbrains.webstore.core.mapper.OrderItemMapper;
-import ru.geekbrains.webstore.core.service.OrderItemService;
-import java.security.Principal;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.geekbrains.webstore.api.dto.CommentDto;
 import ru.geekbrains.webstore.api.dto.OrderItemDto;
+import ru.geekbrains.webstore.core.mapper.OrderItemMapper;
+import ru.geekbrains.webstore.core.service.OrderItemService;
 
 @RestController
 @AllArgsConstructor
@@ -22,12 +21,12 @@ public class OrderItemController {
   private OrderItemService orderItemService;
 
   @GetMapping("/check/{productId}")
-  public OrderItemDto checkComments(@PathVariable Long productId, Principal principal) {
-    return orderItemService.checkComments(productId, principal);
+  public OrderItemDto checkComments(@PathVariable Long productId, @RequestHeader String username) {
+    return orderItemService.checkComments(productId, username);
   }
 
   @PutMapping
-  public OrderItemDto updateOrderItem(@RequestBody CommentDto commentDto, Principal principal) {
-    return OrderItemMapper.ORDER_ITEM_MAPPER.fromOrderItem(orderItemService.updateComment(commentDto, principal));
+  public OrderItemDto updateOrderItem(@RequestBody CommentDto commentDto, @RequestHeader String username) {
+    return OrderItemMapper.ORDER_ITEM_MAPPER.fromOrderItem(orderItemService.updateComment(commentDto, username));
   }
 }
--- core-service/src/main/java/ru/geekbrains/webstore/core/service/OrderItemService.java
+++ core-service/src/main/java/ru/geekbrains/webstore/core/service/OrderItemService.java
@@ -1,10 +1,5 @@
 package ru.geekbrains.webstore.core.service;
 
-import ru.geekbrains.webstore.core.entity.Comment;
-import ru.geekbrains.webstore.core.entity.OrderItem;
-import ru.geekbrains.webstore.core.mapper.CommentMapper;
-import ru.geekbrains.webstore.core.mapper.OrderMapper;
-import java.security.Principal;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +7,10 @@ import ru.geekbrains.webstore.api.dto.CommentDto;
 import ru.geekbrains.webstore.api.dto.OrderDto;
 import ru.geekbrains.webstore.api.dto.OrderItemDto;
 import ru.geekbrains.webstore.api.exception.ResourceNotFoundException;
+import ru.geekbrains.webstore.core.entity.Comment;
+import ru.geekbrains.webstore.core.entity.OrderItem;
+import ru.geekbrains.webstore.core.mapper.CommentMapper;
+import ru.geekbrains.webstore.core.mapper.OrderMapper;
 import ru.geekbrains.webstore.core.repository.OrderItemRepository;
 
 @Service
@@ -21,17 +20,17 @@ public class OrderItemService {
   private OrderItemRepository orderItemRepository;
   private OrderService orderService;
 
-  public OrderItem updateComment(CommentDto commentDto, Principal principal) {
+  public OrderItem updateComment(CommentDto commentDto, String username) {
     Long id = commentDto.getOrderItemId();
     OrderItem orderItem = orderItemRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("OrderItem id = " + id + " not found"));
-    commentDto.setUsername(principal.getName());
+    commentDto.setUsername(username);
     Comment comment = CommentMapper.COMMENT_MAPPER.toComment(commentDto, this);
     orderItem.setComment(comment);
     return orderItemRepository.save(orderItem);
   }
 
-  public OrderItemDto checkComments(Long productId, Principal principal) {
-    List<OrderDto> orders = OrderMapper.ORDER_MAPPER.toOrderDtoList(orderService.findAllByUsername(principal.getName()));
+  public OrderItemDto checkComments(Long productId, String username) {
+    List<OrderDto> orders = OrderMapper.ORDER_MAPPER.toOrderDtoList(orderService.findAllByUsername(username));
     for (OrderDto order : orders) {
       for (OrderItemDto item : order.getItems()) {
         if (item.getProductId().equals(productId) && item.getCommentDto() == null) {
